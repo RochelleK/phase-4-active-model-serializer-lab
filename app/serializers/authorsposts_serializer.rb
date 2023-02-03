d4adeb40d6efa11f2e29e4481c4f049dcde05769
Rochelle Katzman <zelrochelle@gmail.com>
2023-02-03 13:59:34 -0500

    commit tree b2668be39ce61c43f94fb47f745036668567f695

---
+++ app/serializers/authorsposts_serializer.rb
@@ -0,0 +1,10 @@
+class AuthorspostsSerializer < ActiveModel::Serializer
+  attributes :title, :short_content, :tags
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end 
+
+  belongs_to :author
+  has_many :tags
+end
